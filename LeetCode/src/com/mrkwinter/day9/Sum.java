c3045895aa5e863e08ddfbb66497a0d6d373a06a
MrkWinter <744988414@qq.com>
2023-08-20 10:33:28 +0800

    每日一题

---
+++ LeetCode/src/com/mrkwinter/day9/Sum.java
@@ -0,0 +1,12 @@
+package com.mrkwinter.day9;
+
+/**
+ * @author MrkWinter
+ * @version 1.0
+ * 2235. 两整数相加
+ */
+public class Sum {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
